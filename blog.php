5926e29fdb5492baeaefde9c7dbbe23ccc81a6e1
Eriona-Kosumi <76102370+Eriona-Kosumi@users.noreply.github.com>
2021-05-21 23:25:18 +0200

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,344 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Go Trip </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- <link rel="manifest" href="site.webmanifest"> -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+    <!-- Place favicon.ico in the root directory -->
+    
+  <!-- CSS here -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="assets/css/slicknav.css">
+  <link rel="stylesheet" href="assets/css/animate.min.css">
+  <link rel="stylesheet" href="assets/css/magnific-popup.css">
+  <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+  <link rel="stylesheet" href="assets/css/themify-icons.css">
+  <link rel="stylesheet" href="assets/css/themify-icons.css">
+  <link rel="stylesheet" href="assets/css/slick.css">
+  <link rel="stylesheet" href="assets/css/nice-select.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+    <!-- Preloader Start -->
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="assets/img/logo/logo.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Preloader Start-->
+    <header>
+        <!-- Header Start -->
+       <div class="header-area">
+            <div class="main-header ">
+                <div class="header-top top-bg d-none d-lg-block">
+                   <div class="container">
+                    <div class="row justify-content-between align-items-center">
+                        <div class="col-lg-8">
+                            <div class="header-info-left">
+                                <ul>                          
+                                    <li>needhelp@gotrip.com</li>
+                                    <li>+377 45 *** ***</li>
+                                    <li>Pristina, Kosovo</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="header-info-right f-right">
+                                <ul class="header-social">    
+                                    <li><a href=https://twitter.com/login><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a></li>
+                                    <li><a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a></li>
+                                   <li> <a href="https://www.pinterest.com/"><i class="fab fa-pinterest-p"></i></a></li>
+                            </div>
+                        </div>
+                       </div>
+                   </div>
+                </div>
+               <div class="header-bottom  header-sticky">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <!-- Logo -->
+                            <div class="col-xl-2 col-lg-2 col-md-1">
+                                <div class="logo">
+                                  <a href="indexi.php"><img src="assets/img/logo/logo.png" alt=""></a>
+                                </div>
+                            </div>
+                            <div class="col-xl-10 col-lg-10 col-md-10">
+                                <!-- Main-menu -->
+                                <div class="main-menu f-right d-none d-lg-block">
+                                    <nav>               
+                                        <ul id="navigation">                                                                                                                                     
+                                           <li><a href="indexi.php">Home</a></li>
+                                            <li><a href="aboutus.php">About US</a></li>
+                                            <li><a href="blog.html">Blog</a>
+                                                <ul class="submenu">
+                                                    <li><a href="elements.html">Best of</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="login1.php">Log In</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                            <!-- Mobile Menu -->
+                            <div class="col-12">
+                                <div class="mobile_menu d-block d-lg-none"></div>
+                            </div>
+                        </div>
+                    </div>
+               </div>
+            </div>
+       </div>
+        <!-- Header End -->
+    </header>
+  
+     <!-- slider Area Start-->
+     <div class="slider-area ">
+        <!-- Mobile Menu -->
+        <div class="single-slider slider-height2 d-flex align-items-center" data-background="assets/img/hero/hero12.jpg">
+        </div>
+     </div>
+     <!-- slider Area End-->
+    <!--================Blog Area =================-->
+    <section class="blog_area section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mb-5 mb-lg-0">
+                    <div class="blog_left_sidebar">
+                        <article class="blog_item">
+                            <div class="blog_item_img">
+                                <img class="card-img rounded-0" src="assets/img/blog/single_blog_1.png" alt="">
+                                <a href="https://www.timeanddate.com/calendar/" class="blog_item_date">
+                                    <h3>17</h3>
+                                    <p>May</p>
+                                </a>
+                            </div>
+
+                            <div class="blog_details">
+                                <a class="d-inline-block" href="https://www.wildandaway.com/">
+                                    <h2>Get Ready for a Party Travel Adventure</h2>
+                                </a>
+                                <p>We work hard, so let's play hard! Pack your bags and get ready for an adventure! Grab your friends and make new ones but get ready to have a rockin' time. Get ready for a vacation with a wonderful mix of adventure, party and relaxation with like minded people.<br>Music is always an adventure. Mostly it’s an adventure in your mind as you weave in and out of lyrics and music stanzas, with gentle staccato and mellow moments offset by passionate crescendos and a multitude of experiences.</p>
+                            </div>
+                        </article>
+
+                        <article class="blog_item">
+                            <div class="blog_item_img">
+                                <img class="card-img rounded-0" src="assets/img/blog/single_blog_2.png" alt="">
+                                <a href="https://www.timeanddate.com/calendar/" class="blog_item_date">
+                                    <h3>15</h3>
+                                    <p>July</p>
+                                </a>
+                            </div>
+
+                            <div class="blog_details">
+                                <a class="d-inline-block" href="https://www.nationalgeographic.com/photography/photo-tips/travel-photography-tips/">
+                                    <h2>Travel photography</h2>
+                                </a>
+                                <p>Travel photography is a genre of photography that may involve the documentation of an area's landscape, people, cultures, customs and history. The Photographic defines a travel photo as an image that expresses the feeling of a time and place, portrays a land, its people, or a culture in its natural state, and has no geographical limitations.</p>
+                            </div>
+                        </article>
+                        <article class="blog_item">
+                            <div class="blog_item_img">
+                                <img class="card-img rounded-0" src="assets/img/blog/single_blog_4.png" alt="">
+                                <a href="https://www.timeanddate.com/calendar/"class="blog_item_date">
+                                    <h3>5</h3>
+                                    <p>Jan</p>
+                                </a>
+                            </div>
+
+                            <div class="blog_details">
+                                <a class="d-inline-block" href="https://www.wildandaway.com/">
+                                    <h2>Through our travels we have come up with a number of reasons why we believe… </h2>
+                                </a>
+                                <p>The memories that you and your partner will create on your travels before you’re married are unique and will last a lifetime. Dating life is obviously quite different than married life, and your adventures are likely going to be as well.</p>
+                            </div>
+                        </article>
+
+                        <article class="blog_item">
+                            <div class="blog_item_img">
+                                <img class="card-img rounded-0" src="assets/img/blog/single_blog_5.png" alt="">
+                                <a href="https://www.timeanddate.com/calendar/" class="blog_item_date">
+                                    <h3>18</h3>
+                                    <p>Sept</p>
+                                </a>
+                            </div>
+
+                            <div class="blog_details">
+                                <a class="d-inline-block" href="https://www.travelrepublic.co.uk/blog/15-places-to-celebrate-your-birthday">
+                                    <h2>How Do You Celebrate Your Birthday While Traveling?</h2>
+                                </a>
+                                <p>Congratulations! You can no longer claim youthful ignorance to justify poor life decisions. After all, you are in your 30s now, and the things that once thrilled your twenty-year-old self have likely given way to being a full-blown adult.But just because you’re older now and plagued by a wealth of responsibility, that doesn’t mean you can’t take an epic birthday vacation. The only question is: where should you go?<br>All of the best birthday vacations start with a few good friends. Round them up, gather their input and see what everyone has in mind.</p>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                 <div class="col-lg-4">
+                    <div class="blog_right_sidebar">
+                        <?php
+                        include "index.php";
+                        ?>
+                    </div> 
+                </div>
+            
+    <!--================Blog Area =================-->
+        <!-- Footer Start-->
+        <div class="footer-area footer-padding footer-bg" data-background="assets/img/service/footer_bg.jpg">
+            <div class="container">
+                <div class="row d-flex justify-content-between">
+                    <div class="col-xl-3 col-lg-3 col-md-5 col-sm-6">
+                       <div class="single-footer-caption mb-50">
+                         <div class="single-footer-caption mb-30">
+                              <!-- logo -->
+                             <div class="footer-logo">
+                                 <a href="indexi.php"><img src="assets/img/logo/logo2_footer.png" alt=""></a>
+                             </div>
+                             <div class="footer-tittle">
+                                 <div class="footer-pera">
+                                     <p>Travel Leaders travel agents will take the time to understand your unique travel interests and aspirations, until we "get you." We then use our education, first-hand travel experience and relationships with airlines, car, hotel, resort, tour and cruise supplier partners to make sure you enjoy an exceptional personalized travel experience that will create memories to last a lifetime.</p>
+                                </div>
+                             </div>
+                         </div>
+                       </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-5">
+                        <div class="single-footer-caption mb-50">
+                            <div class="footer-tittle">
+                                <h4>Quick Links to our partners</h4>
+                                <ul>
+                                    <li><a href="http://zenithtravel.al/paketa-turistike/"> Zenith Travel</a></li>
+                                    <li><a href="https://www.trivago.rs/"> Trivago Hotel</a></li>
+                                    <li><a href="http://www.quincerestaurant.com/">Quince Restaurant</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-7">
+                        <div class="single-footer-caption mb-50">
+                            <div class="footer-tittle">
+                                <h4>New</h4>
+                                <ul>
+                                    <li><a href="https://www.travelandleisure.com/style/fashion/best-travel-clothes-for-women-men">Woman Cloth</a></li>
+                                    <li><a href="https://www.smartertravel.com/7-must-fashion-accessories-fall-travel/">Fashion Accessories</a></li>
+                                    <li><a href="https://www.nomadicmatt.com/travel-blogs/61-travel-tips/"> 61 Travel Tips</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-5 col-sm-7">
+                        <div class="single-footer-caption mb-50">
+                            <div class="footer-tittle">
+                                <h4>Support</h4>
+                                <ul>
+                                 <li><a href="#">Terms & Conditions</a></li>
+                                 <li><a href="#">Privacy Policy</a></li>
+                                 <li><a href="#">Privacy Policy</a></li>
+                                 <li><a href="#">Report a Payment Issue</a></li>
+                             </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Footer bottom -->
+                <div class="row pt-padding">
+                 <div class="col-xl-7 col-lg-7 col-md-7">
+                    <div class="footer-copy-right">
+                         <p>
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved<i class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Go Trip</a>
+  </p>
+                    </div>
+                 </div>
+                  <div class="col-xl-5 col-lg-5 col-md-5">
+                        <!-- social -->
+                        <div class="footer-social f-right">
+                            <a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+                            <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                        </div>
+                 </div>
+             </div>
+            </div>
+        </div>
+        <!-- Footer End-->
+    </footer>
+
+    <!-- JS here -->
+    
+        <!-- All JS Custom Plugins Link Here here -->
+        <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+        
+        <!-- Jquery, Popper, Bootstrap -->
+        <script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="./assets/js/popper.min.js"></script>
+        <script src="./assets/js/bootstrap.min.js"></script>
+        <!-- Jquery Mobile Menu -->
+        <script src="./assets/js/jquery.slicknav.min.js"></script>
+
+        <!-- Jquery Slick , Owl-Carousel Plugins -->
+        <script src="./assets/js/owl.carousel.min.js"></script>
+        <script src="./assets/js/slick.min.js"></script>
+        <!-- One Page, Animated-HeadLin -->
+        <script src="./assets/js/wow.min.js"></script>
+        <script src="./assets/js/animated.headline.js"></script>
+        <script src="./assets/js/jquery.magnific-popup.js"></script>
+
+        <!-- Scrollup, nice-select, sticky -->
+        <script src="./assets/js/jquery.scrollUp.min.js"></script>
+        <script src="./assets/js/jquery.nice-select.min.js"></script>
+        <script src="./assets/js/jquery.sticky.js"></script>
+        
+        <!-- contact js -->
+        <script src="./assets/js/contact.js"></script>
+        <script src="./assets/js/jquery.form.js"></script>
+        <script src="./assets/js/jquery.validate.min.js"></script>
+        <script src="./assets/js/mail-script.js"></script>
+        <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+        
+        <!-- Jquery Plugins, main Jquery -->    
+        <script src="./assets/js/plugins.js"></script>
+        <script src="./assets/js/main.js"></script>
+         <script>
+            loadTrip();
+            function loadTrip(){
+            var dataHandler = $("#load-data-here");
+            dataHandler.html("");
+
+            $.ajax({
+                type : "GET",
+                data : "",
+                url : "getTrip.php",
+                success : function(result){
+                    var resultObj = JSON.parse(result);
+
+                    $.each(resultObj,function(key,val){
+                        var newRow = $("<tr>");
+                        newRow.html("<td>"+val.tripID+"</td><td>"
+                        +val.place+"</td><td>"+val.start+"</td><td>"+
+                        val.end+"</td><td>"+val.description+
+                        "</td><td><button onclick='changeColor(this)'' class='selectTrip' id='"+
+                        val.tripID+"'>Select</button></td>");
+                        
+                        dataHandler.append(newRow);
+                        
+                    })
+                }
+            })
+        }
+
+        </script>
+        
+    </body>
+</html>
